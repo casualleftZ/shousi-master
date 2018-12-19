7e82089846dbcf0f5c9ea5a665f380e2f019d08d
casualleftZ <1214064362@qq.com>
2018-12-19 18:16:03 +0800

    指纹识别

--- app/src/main/java/comqwera/mingrisoft/shousi/activity/activity/GoodsActivity.java
+++ app/src/main/java/comqwera/mingrisoft/shousi/activity/activity/GoodsActivity.java
@@ -1,16 +1,29 @@
 package comqwera.mingrisoft.shousi.activity.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.io.File;
 
 public class GoodsActivity extends AppCompatActivity {
-
+    private ImageButton ib_fanhui;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods);
+        ib_fanhui=(ImageButton)findViewById(R.id.ib_fanhui);
+        ib_fanhui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(GoodsActivity.this,MainActivity.class);
+                startActivity(a);
+                GoodsActivity.this.finish();
+            }
+        });
 
     }
 }
