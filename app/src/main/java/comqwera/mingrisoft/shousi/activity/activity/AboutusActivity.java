132fcbd5f77049a2f93e07473e16b5632d0c564c
zhangxunice <2462832901@qq.com>
2019-01-02 16:55:22 +0800

    增加了关于我们

---
+++ app/src/main/java/comqwera/mingrisoft/shousi/activity/activity/AboutusActivity.java
@@ -0,0 +1,14 @@
+package comqwera.mingrisoft.shousi.activity.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutusActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
+}
